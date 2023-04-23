af9097707de741c771116e5a6a3a7d9473775968
donald <aliendonald@gmail.com>
2023-04-23 15:13:35 +0200

    dap add python js

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -62,6 +62,8 @@ return {
         -- You'll need to check that you have the required things installed
         -- online, please don't ask me how to install them :)
         ensure_installed = {
+          "python",
+          "js",
           -- Update this to ensure that you have the debuggers for the langs you want
         },
       },

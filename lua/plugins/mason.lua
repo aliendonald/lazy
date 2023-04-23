7ef4b48a56a4222d44c645c13dff5d21177d3baf
donald <aliendonald@gmail.com>
2023-04-23 15:13:51 +0200

    add pyright

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,7 +3,15 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "prettierd")
+      table.insert(opts.ensure_installed, "flake8")
     end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = {
+      ensure_installed = { "lua_ls", "pyright" },
+    },
   },
   {
     "jay-babu/mason-null-ls.nvim",

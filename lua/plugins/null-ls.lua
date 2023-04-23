b9cde3baa2328c69ba213f71c07a77624074c9cf
donald <aliendonald@gmail.com>
2023-04-23 15:14:05 +0200

    add black and flake8

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,6 +4,8 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.prettierd)
+      table.insert(opts.sources, nls.builtins.formatting.black)
+      table.insert(opts.sources, nls.builtins.diagnostics.flake8)
     end,
   },
 }

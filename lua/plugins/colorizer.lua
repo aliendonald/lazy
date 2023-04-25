faa536fe4f2a4b272fc3a0590148df1889db000e
donald <aliendonald@gmail.com>
2023-04-25 22:44:06 +0200

    colorizer

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,14 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  -- init = function()
+  -- require("core.utils").lazy_load "nvim-colorizer.lua"
+  -- end,
+  config = function(_, opts)
+    require("colorizer").setup(opts)
+
+    -- execute colorizer as soon as possible
+    vim.defer_fn(function()
+      require("colorizer").attach_to_buffer(0)
+    end, 0)
+  end,
+}
